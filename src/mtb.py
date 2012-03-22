b222f0632896bf2696c820da28395afe788d6db2
Paul Pritchard <paulpritchard68@gmail.com>
2012-03-22 16:28:04 +0100

    Better helptext with argparse

--- src/mtb.py
+++ src/mtb.py
@@ -18,31 +18,33 @@
  with this program.  If not, see <http://www.gnu.org/licenses/>."""
 
 import sys
-import getopt
+import argparse
 from mtbxml import Transitions
 from mtbhelp import mtb_help
 
 XML_DOC = Transitions()
 
 def main():
-    """The Main Procedure"""
+    """ The Main Procedure
+        Parses the options and executes the build function"""
+
+    parser = argparse.ArgumentParser(description='Create a transitioning background XML for Gnome 2')
+    parser.add_argument('-p', '--path', action='store', help='Set path for images and XML')
+    parser.add_argument('-s', '--static', action='store', help='Set static time in seconds')
+    parser.add_argument('-t', '--transition', action='store', help='Set transiton time in seconds')
+
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hp:s:t:", ["help", "path=", "static=", "transition="])
-    except getopt.GetoptError, err:
-        print str(err) 
+        options = parser.parse_args()
+    except:
         sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            mtb_help()
-            return
-        elif opt in ("-p", "--path"):
-            XML_DOC.set_path(arg)
-        elif opt in ("-s", "--static"):
-            XML_DOC.set_static(arg)
-        elif opt in ("-t", "--transition"):
-            XML_DOC.set_transition(arg)
-        else:
-            assert False, "unhandled option"
+
+    if options.path is not None:
+        XML_DOC.set_path(options.path)
+    if options.static is not None:
+        XML_DOC.set_static(options.static)
+    if options.transition is not None:
+        XML_DOC.set_transition(options.transition)
+    
     XML_DOC.build()
 
 if (__name__=="__main__"):
