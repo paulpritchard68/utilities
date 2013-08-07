39a4b52707f65b829743440a5084d891b72c9ee0
ExpatPaul <paulpritchard68@gmail.com>
2013-08-07 22:16:08 +0200

    Remove version number from boilerplate

--- mtb.py
+++ mtb.py
@@ -4,12 +4,12 @@
  mtb.py
  Copyright (C) Paul Pritchard 2013 
  
- MTB3 is free software: you can redistribute it and/or modify it under the 
+ MTB is free software: you can redistribute it and/or modify it under the 
  terms of the GNU General Public License as published by the Free Software 
  Foundation, either version 3 of the License, or (at your option) any 
  later version.
  
- MTB3 is distributed in the hope that it will be useful, but WITHOUT ANY 
+ MTB is distributed in the hope that it will be useful, but WITHOUT ANY 
  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  
