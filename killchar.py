82659ebad93109cc9f53000a48b4c9d6b606d531
ExpatPaul <paulpritchard68@gmail.com>
2014-10-05 21:17:07 +0200

    Another annoying character killed

--- killchar.py
+++ killchar.py
@@ -35,6 +35,7 @@ def rename_files():
         newfile = re.sub('__', '_', newfile)
         newfile = re.sub('\.{3}', '', newfile)
         newfile = re.sub('\._', '_', newfile)
+        newfile = re.sub('"', '', newfile)
         os.rename(filename, newfile)
 
 if __name__ == "__main__":
